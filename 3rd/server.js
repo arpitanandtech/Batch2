921868602b1a5018a8732344f05cc1b8b3eb6962
arpitanandtech <arpitanandtech@gmail.com>
2025-01-27 22:12:04 +0530

    Feat: 27 jan

--- 3rd/server.js
+++ 3rd/server.js
@@ -2,9 +2,13 @@ const express = require('express');
 
 const bodyParser = require('body-parser');
 
+const cors = require("cors");
+
 const router = require("./routes/index.routes");
 
 const app = express();
+
+app.use(cors());
 
 app.use(bodyParser.json()); // Middleware
 
