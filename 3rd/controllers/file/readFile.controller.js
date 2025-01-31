6a3e3ab2944ba73d9039e9c084f3159aab73c5d7
arpitanandtech <arpitanandtech@gmail.com>
2025-01-31 22:26:54 +0530

    Feat: 27 jan

--- 3rd/controllers/file/readFile.controller.js
+++ 3rd/controllers/file/readFile.controller.js
@@ -1,12 +1,19 @@
 const fs = require('fs');
+const path = require('path');
 
 const fileReadController = function (req, res, next) {
-  fs.readFile("../../data/data.txt", function (err, data) {
+
+  console.log(process.cwd());
+
+  const p = path.join(process.cwd() + "/data/data.txt")
+
+  fs.readFile(p, "utf8", function (err, data) {
     if (err) {
       console.log(err);
     }
     console.log(data);
   });
+  res.json("ok")
 }
 
 module.exports = fileReadController;
